829bf6246bcc0fc621138e18804f4c3a8713dc97
shailesh-bst <shailesh.raghav@bluestacks.com>
2021-07-04 22:30:58 +0530

    adding config

---
+++ Assignment/AppLogin/config.py
@@ -0,0 +1,18 @@
+
+
+desired_cap_playstore = {
+    "deviceName": "emulator-5554",
+    "platformName": "Android",
+    "appPackage": "com.android.vending",
+    "appActivity": "com.google.android.finsky.activities.MainActivity",
+    "appWaitDuration": 40000
+}
+
+
+desired_cap_gametv = {
+    "deviceName": "emulator-5554",
+    "platformName": "Android",
+    "appPackage": "tv.game",
+    "appActivity": "tv.game.MainActivity",
+    "appWaitDuration": 40000
+}
