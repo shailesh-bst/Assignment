df5e56976c7586356a5bb75c5021f2ea2983876c
shailesh-bst <shailesh.raghav@bluestacks.com>
2021-07-05 17:29:29 +0530

    Update main.py

--- Assignment/Wheater_reporting/main.py
+++ Assignment/Wheater_reporting/main.py
@@ -15,20 +15,30 @@ def compare_temperatures(temp_website, temp_api):
 def assert_temperature_variance(temp_website, temp_api, variance):
     for i in range(0, len(temp_website)):
         assert abs(temp_website[i] - temp_api[i]) <= variance
-        print(i + "In range")
+        print("In range")
 
 
 if __name__ == "__main__":
     # getting values from user input file
     input_user = user_input.get_user_input()
     if input_user is not None:
-        # Fetching values from respective sources
-        temperatures_of_cities_website = np.array(website.get_temp_site1(input_user[0]))
-        temperatures_of_cities_API = np.array(rest_api_weather.get_temp_site2(input_user[0]))
-        # Comparing two source temperatures
-        compare_temperatures(temperatures_of_cities_website, temperatures_of_cities_API)
-        # Checking variance
-        assert_temperature_variance(temperatures_of_cities_website, temperatures_of_cities_API, input_user[1])
+        user_city_list = input_user[0]
+        user_variance_input = input_user[1]
 
-    else:print("User values were not received")
+        # Fetching values from respective sources
+        temperatures_of_cities_website = np.array(website.get_temp_site1(user_city_list))
+        temperatures_of_cities_API = np.array(rest_api_weather.get_temp_site2(user_city_list))
+
+        if temperatures_of_cities_API is not None and temperatures_of_cities_API is not None:
+            # Comparing two source temperatures
+            compare_temperatures(temperatures_of_cities_website, temperatures_of_cities_API)
+
+            # Checking variance
+            assert_temperature_variance(temperatures_of_cities_website, temperatures_of_cities_API, user_variance_input)
+
+        else:
+            print("Either or both sources returned None")
+
+    else:
+        print("User values were not received")
 
