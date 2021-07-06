27347ce46bfb553fd0f84a605cf1ad0eb399a656
shailesh-bst <shailesh.raghav@bluestacks.com>
2021-07-06 09:56:08 +0530

    Update website.py

--- Assignment/Wheater_reporting/website.py
+++ Assignment/Wheater_reporting/website.py
@@ -5,6 +5,7 @@ import config
 
 def get_temp_site1(loc_list):
     ''' TO get the temperature openweather webelements '''
+
     try:
         # Final list of temperatures of cities to be returned
         temperature_list = []
@@ -12,9 +13,10 @@ def get_temp_site1(loc_list):
         driver.get("https://weather.com/")
         if driver.find_element_by_id("LocationSearch_input").is_displayed():
             config.logger.info("Website is up and working")
-            driver.maximize_window()
 
-            time.sleep(3)
+            driver.maximize_window()
+            time.sleep(2)
+
             # Calculating temperatures
             for i in range(0, len(loc_list)):
                 input_city = driver.find_element_by_id("LocationSearch_input")
@@ -27,9 +29,9 @@ def get_temp_site1(loc_list):
                 temp = driver.find_element_by_xpath(
                     "//*[@id='WxuCurrentConditions-main-b3094163-ef75-4558-8d9a-e35e6b9b1034']/div/section/div/div[2]/div[1]/span").text
                 temperature_list.append(int(temp[:2]))
+                config.logger.info(str(loc_list[i]) + " temperature is captured")
                 driver.back()
                 time.sleep(2)
-                config.logger.info(str(i) + " temperature is captured")
 
             if len(temperature_list) > 0:
                 return temperature_list
